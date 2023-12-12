d8cda632d596ee873f048b5576e2eaafc8f7afc7
Ste79ITA <nastri.s@gmail.com>
2023-12-12 16:10:59 +0100

    movies displayed in homepage

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $movies = Movie::all();
+
+        return view('index', compact('movies'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,18 @@
 <div class="container">
     <h1>HOMEPAGE</h1>
 
-    <ul class="movies">
-        <li class="movie">MOVIE</li>
-    </ul>
+    <div class="movies d-flex">
+        @foreach ($movies as $movie)
+            <div class="card col-6 align-items-start">
+                <div class="movie">Titolo: {{ $movie->title }}</div>
+                <div class="movie">Titolo Originale: {{ $movie->original_title }}</div>
+                <div class="movie">Nazione: {{ $movie->nationality }}</div>
+                <div class="movie">Data: {{ $movie->date }}</div>
+                <div class="movie">Vote: {{ $movie->vote }}</div>
+
+            </div>
+        @endforeach
+    </div>
 </div>
 </section>
 @endsection
