9d4312d35463807c67d61090b8624ac80573572a
Ste79ITA <nastri.s@gmail.com>
2023-12-12 17:03:53 +0100

    Page styled

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
+@section('title', "Movie's Page")
+
 @section('content')
 <section class="py-5">
-<div class="container">
-    <h1>HOMEPAGE</h1>
+<div class="container ">
+    <h1 class="title py-5">Movies</h1>
 
-    <div class="movies d-flex">
+    <div class="movies d-flex flex-wrap">
         @foreach ($movies as $movie)
-            <div class="card col-6 align-items-start">
+            <div class="card col-2 p-3 m-3 gap-3">
                 <div class="movie">Titolo: {{ $movie->title }}</div>
                 <div class="movie">Titolo Originale: {{ $movie->original_title }}</div>
                 <div class="movie">Nazione: {{ $movie->nationality }}</div>
