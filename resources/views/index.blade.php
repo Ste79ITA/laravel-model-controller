90d0a07a2db0d5ecdd3b8d572dc16a4da00f81cc
Ste79ITA <nastri.s@gmail.com>
2023-12-12 15:40:52 +0100

    added new controller PageController and the route for homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="py-5">
+<div class="container">
+    <h1>HOMEPAGE</h1>
+
+    <ul class="movies">
+        <li class="movie">MOVIE</li>
+    </ul>
+</div>
+</section>
+@endsection
